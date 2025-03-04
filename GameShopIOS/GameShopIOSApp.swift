660244bfd240ff393f8fbd16b33094583c7261d3
Usuario invitado <Guest@B213-21.local>
2025-03-04 18:55:30 +0100

    primer commit

---
+++ GameShopIOS/GameShopIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameShopIOSApp.swift
+//  GameShopIOS
+//
+//  Created by Usuario invitado on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct GameShopIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GameShopIOSTests/GameShopIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameShopIOSTests.swift
+//  GameShopIOSTests
+//
+//  Created by Usuario invitado on 4/3/25.
+//
+
+import Testing
+@testable import GameShopIOS
+
+struct GameShopIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
